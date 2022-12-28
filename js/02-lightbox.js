2caabe9cd20904baecb0cb4f8c769bc0605598ab
nastya-koptilova <koptilova.nastya@gmail.com>
2022-12-28 14:16:10 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeGalleryImg = ({ preview, original, description } = {}) => {
+  return `
+      <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt="${description}">
+      </a>
+  `;
+};
+
+const galleryCards = galleryItems.map((el) => makeGalleryImg(el));
+
+galleryEl.insertAdjacentHTML("beforeend", galleryCards.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+galleryEl.addEventListener("click", onGalleryElClick);
